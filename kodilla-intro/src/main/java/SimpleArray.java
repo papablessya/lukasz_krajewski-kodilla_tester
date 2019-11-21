005fa46ef74c947b72dc2667ab54545f0823a351
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2019-11-22 00:07:03 +0100

    AltMaster Project

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,17 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+
+        String[] names = new String[5];
+        names[0] = "John";
+        names[1] = "Jeremy";
+        names[2] = "Jonah";
+        names[3] = "Ed";
+        names[4] = "Adam";
+
+        String name = names[3];
+        System.out.println(name);
+
+        int numberOfElements = names.length;
+        System.out.println(numberOfElements);
+    }
+}
