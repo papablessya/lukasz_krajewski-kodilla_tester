45bd0d2e904c95573af06e33f3fd650d4f075633
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2019-11-28 12:58:55 +0100

    AltMaster Project

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,4 +1,29 @@
+import java.util.Random;
 public class RandomNumbers {
+
+    int numbers;
+
+    public static void main(String[] args) {
+        Number[] numbers = new Number[30];
+        int i = 0;
+        while (i <= 30) {
+            System.out.println(i);
+            i++;
+        }
+    }
+    public int getCountOfRandomNumbers(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(5000);
+            sum = sum + temp;
+            result++;
+        }
+        return result;
+    }
 
 
 }
+
+
