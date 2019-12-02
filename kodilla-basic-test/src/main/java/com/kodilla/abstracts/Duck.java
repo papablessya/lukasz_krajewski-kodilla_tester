2fe8356acefae270bac5b6442817d5de71cedc67
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2019-12-02 12:10:58 +0100

    AltMaster Project

---
+++ kodilla-basic-test/src/main/java/com/kodilla/abstracts/Duck.java
@@ -0,0 +1,15 @@
+package com.kodilla.abstracts;
+
+public class Duck extends Animal {
+
+
+    public Duck() {
+        super(2);
+    }
+
+    @Override
+    public void giveVoice() {
+        System.out.println("quack quack");
+
+    }
+}
