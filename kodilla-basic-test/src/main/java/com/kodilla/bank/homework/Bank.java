d72c43807ef54fd685b20b36d47d11872debcd13
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2019-12-16 00:07:37 +0100

    AltMaster Project

--- kodilla-basic-test/src/main/java/com/kodilla/bank/homework/Bank.java
+++ kodilla-basic-test/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -1,45 +1,54 @@
 package com.kodilla.bank.homework;
 
 public class Bank {
-    private String name;
-    private CashMachine atm1;
-    private CashMachine atm2;
-    private CashMachine atm3;
+    private CashMachine[] atms;
 
-    public Bank(String name) {
-        this.name = name;
-        this.atm1 = new CashMachine();
-        this.atm2 = new CashMachine();
-        this.atm3 = new CashMachine();
+
+    public Bank() {
+        atms = new CashMachine[3];
+        atms[0] = new CashMachine();
+        atms[1] = new CashMachine();
+        atms[2] = new CashMachine();
+
     }
 
     public void addAtm1Transaction(int transaction) {
-        this.atm1.add(transaction);
+//        this.atm1.add(transaction);
     }
 
     public void addAtm2Transaction(int transaction) {
-        this.atm2.add(transaction);
+//        this.atm2.add(transaction);
     }
 
     public void addAtm3Transaction(int transaction) {
-        this.atm3.add(transaction);
+//        this.atm3.add(transaction);
     }
 
-    public double getCashIn() {
-        double sum = this.atm1.getCashIn() + this.atm2.getCashIn() + this.atm3.getCashIn();
-        return sum;
+    public int getCashIn() {
+        int cashInCount = 0;
+        for (int i = 0; i < atms.length; i++){
+            cashInCount = atms[i].getCashIn() + cashInCount;
+        }
+        return cashInCount;
     }
-    public double getCashOut() {
-        double sum = this.atm1.getCashOut() + this.atm2.getCashOut() + this.atm3.getCashOut();
-        return sum;
+    public int getCashOut() {
+        int cashOutCount = 0;
+        for (int i = 0; i < atms.length; i++){
+            cashOutCount = atms[i].getCashOut() + cashOutCount;
+        }
+        return cashOutCount;
     }
-    public double getBalance() {
-        double sum = this.atm1.getBalance() + this.atm2.getBalance() + this.atm3.getBalance();
-        return sum;
+    public int getBalance() {
+        int totalBalance = 0;
+        for (int i = 0; i < atms.length; i++){
+            totalBalance = totalBalance + atms[i].getBalance();
+        }
+        return totalBalance;
     }
 
-
-
+    public CashMachine[] getAtms() {
+        return atms;
+    }
 }
 
 
--- kodilla-basic-test/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
+++ kodilla-basic-test/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
@@ -1,4 +1,15 @@
 package com.kodilla.bank.homework;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankTestSuite {
+    @Test
+    public void bankTest(){
+        Bank bank = new Bank();
+        assertEquals(0,bank.getBalance());
+    }
+
+
 }
