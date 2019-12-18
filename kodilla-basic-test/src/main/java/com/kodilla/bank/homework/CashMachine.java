c9c3a7c323d4018978efd65befb0d27bf462ce5e
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2019-12-18 10:01:35 +0100

    AltMaster Project

--- kodilla-basic-test/src/main/java/com/kodilla/bank/homework/CashMachine.java
+++ kodilla-basic-test/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -9,50 +9,58 @@ public class CashMachine {
         this.transactions = new int[0];
         this.size = 0;
     }
+
     public void add(int value) {
         this.size++;
         int[] newTab = new int[this.size];
         System.arraycopy(transactions, 0, newTab, 0, transactions.length);
         newTab[this.size - 1] = value;
         this.transactions = newTab;
-}
+    }
+
     public int[] getTransactions() {
         return transactions;
     }
+
     public int getBalance() {
         if (this.transactions.length == 0) {
             return 0;
         }
         int balance = 0;
-        for(int i = 0; i < this.transactions.length; i++) {
+        for (int i = 0; i < this.transactions.length; i++) {
             balance += this.transactions[i];
         }
-        return balance ;
+        return balance;
 
     }
+
     public int getCashIn() {
         if (this.transactions.length == 0) {
             return 0;
         }
         int cashIn = 0;
-        for(int i = 0; i < this.transactions.length; i++) {
-            cashIn += this.transactions[i];
-            if (this.transactions[i] > 0);
+        for (int i = 0; i < this.transactions.length; i++) {
+            if (this.transactions[i] > 0) {
+                cashIn += this.transactions[i];
+            }
         }
-        return cashIn ;
+        return cashIn;
 
     }
+
     public int getCashOut() {
         if (this.transactions.length == 0) {
             return 0;
         }
         int cashOut = 0;
-        for(int i = 0; i < this.transactions.length; i++) {
-            cashOut += this.transactions[i];
-            if (this.transactions[i] < 0);
+        for (int i = 0; i < this.transactions.length; i++) {
+            if (this.transactions[i] < 0) {
+                cashOut += this.transactions[i];
+            }
         }
-        return cashOut ;
+        return cashOut;
 
     }
 }
+
 
--- kodilla-basic-test/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
+++ kodilla-basic-test/src/test/java/com/kodilla/bank/homework/BankTestSuite.java
@@ -10,6 +10,35 @@ public class BankTestSuite {
         Bank bank = new Bank();
         assertEquals(0,bank.getBalance());
     }
-
+    @Test
+    public void testOnePositiveTransaction() {
+        Bank bank = new Bank();
+        CashMachine[] atms = bank.getAtms();
+        atms[0].add(10);
+        assertEquals(10, bank.getCashIn());
+        assertEquals(10, bank.getBalance());
+        assertEquals(0,bank.getCashOut());
+    }
+    @Test
+    public void testOneNegativeTransaction() {
+        Bank bank = new Bank();
+        CashMachine[] atms = bank.getAtms();
+        atms[1].add(-2);
+        assertEquals(-2,bank.getCashOut());
+        assertEquals(0,bank.getCashIn());
+        assertEquals(-2,bank.getBalance());
+    }
+    @Test
+    public void testDifferentTransactions() {
+        Bank bank = new Bank();
+        CashMachine[] atms = bank.getAtms();
+        atms[0].add(1);
+        atms[1].add(-2);
+        atms[2].add(3);
+        atms[2].add(-1);
+        assertEquals(1,bank.getBalance());
+        assertEquals(4,bank.getCashIn());
+        assertEquals(-3,bank.getCashOut());
+    }
 
 }
