7496d85408364a26ab0a25298e2483a18413c758
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2020-01-13 21:03:31 +0100

    AltMaster Project

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -0,0 +1,29 @@
+package com.kodilla.collections.lists.homework;
+
+import com.kodilla.collections.arrays.homework.CarUtils;
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Mustang;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsListApplication {
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+        Ford ford = new Ford(210);
+        cars.remove(ford);
+        cars.add(new Mustang(260));
+        cars.add(new Opel(140));
+
+        cars.remove(1);
+        cars.remove(ford);
+
+
+        System.out.println(cars.size());
+        for(Car car: cars){
+            System.out.println(car.getSpeed());
+        }
+    }
+}
