4b61ab6000f6174c082b5ff3e0f27d942cb52143
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2020-02-03 21:19:16 +0100

    AltMaster Project

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictonary/EnglishWord.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictonary/EnglishWord.java
@@ -1,5 +1,7 @@
 package com.kodilla.collections.adv.exercises.dictonary;
 
+import java.util.Objects;
+
 public class EnglishWord {
     private PartOfSpeech partOfSpeech;
     private String word;
@@ -10,6 +12,20 @@ public class EnglishWord {
 
     public String getWord() {
         return word;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnglishWord that = (EnglishWord) o;
+        return partOfSpeech == that.partOfSpeech &&
+                Objects.equals(word, that.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(partOfSpeech, word);
     }
 
     public EnglishWord(PartOfSpeech partOfSpeech, String word) {
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -0,0 +1,9 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import java.util.List;
+
+public class FlightFinder {
+    public  List<Flight> findFlightsFrom(String departure){}
+
+    public List<Flight> findFlightsTo(String arrival){}
+}
