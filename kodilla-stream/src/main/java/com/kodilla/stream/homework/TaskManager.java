9cd654980e3b253760f4782516c7ece455bad236
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2020-02-10 10:53:31 +0100

    AltMaster Project

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -1,9 +1,22 @@
 package com.kodilla.stream.homework;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TaskManager {
     public static void main(String[] args) {
-    TaskRepository.getTasks();
-    .stream();
-    .filter(u -> )
+        List<String> tasks = TaskRepository.getTasks()
+                .stream()
+                .filter(u -> u.getDeadline().equals(LocalDate.of(2020, 2, 10)))
+                        .map(TaskManager::getTask)
+                        .collect(Collectors.toList());
+        System.out.println(tasks);
+
+
+    }
+
+    public static String getTask(Task task) {
+        return task.getName();
     }
 }
