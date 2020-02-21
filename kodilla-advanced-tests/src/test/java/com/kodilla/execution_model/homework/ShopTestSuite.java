c64d4a776197d692ff7a610577916a76071bd37b
Lukasz Krajewski <lukaskrajewski123@gmail.com>
2020-02-21 11:50:49 +0100

    AltMaster

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -0,0 +1,63 @@
+package com.kodilla.execution_model.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopTestSuite {
+    Shop shop = new Shop();
+    Order bike = new Order("John123", 890.5, LocalDate.of(2020, 1, 12));
+    Order suitcase = new Order("OlivierX", 90.5, LocalDate.of(2020, 2, 22));
+    Order table = new Order("SULTAN", 2890.5, LocalDate.of(2019, 11, 29));
+    Order armchair = new Order("SULTAN", 999.1, LocalDate.of(2019, 11, 29));
+    Order boots = new Order("Ike_Spike", 290.5, LocalDate.of(2020, 2, 17));
+
+
+    @Test
+    public void returnListOfOrdersBetweenTwoDates() {
+        List<LocalDate> orders = Shop.getOrdersListBetweenTwoDates(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 2, 21));
+        assertEquals(bike, orders);
+        assertEquals(suitcase, orders);
+        assertEquals(boots, orders);
+    }
+
+    @Test
+    public void shouldShowMinValueOrder() {
+        List<Order> minValueOrder = Shop.getMin(90.5);
+        Assertions.assertEquals(90.5, minValueOrder);
+
+
+    }
+
+    @Test
+    public void shouldShowMaxValueOrder() {
+        List<Order> maxValueOrder = Shop.getMax(90.5);
+        Assertions.assertEquals(90.5, maxValueOrder);
+    }
+
+    @Test
+    public void returnNumberOfOrders() {
+        int numberOfOrders = shop.getSize();
+        assertEquals(5, numberOfOrders);
+    }
+    @Test
+    public void sumValueOfAllOrders(){
+//        List<Order> sumValueOfOrders = Shop.getSum(Order::getValue);
+    }
+
+
+    @BeforeEach
+    public void initializeOrder() {
+        shop.addOrder(bike);
+        shop.addOrder(suitcase);
+        shop.addOrder(table);
+        shop.addOrder(armchair);
+        shop.addOrder(boots);
+    }
+}
